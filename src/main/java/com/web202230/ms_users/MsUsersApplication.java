cfaa9b093d87de197c516f8a790c9c841e14bb6d
Edwin Vesga <43819230+Vesgae@users.noreply.github.com>
2022-10-12 12:24:54 -0500

    FIRST COMMIT - MS users

---
+++ src/main/java/com/web202230/ms_users/MsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.web202230.ms_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web202230/ms_users/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web202230.ms_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
